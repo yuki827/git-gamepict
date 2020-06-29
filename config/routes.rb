dc41db8950b5649c18468fc3de8237ef106e5117
EC2 Default User <ec2-user@ip-172-31-2-46.ap-northeast-1.compute.internal>
2020-06-29 13:22:33 +0000

    新規登録画面のためのnewアクション設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'posts' => 'posts#index'
+  get   'posts/new'  =>  'posts#new'       #ツイート投稿画面
 end
