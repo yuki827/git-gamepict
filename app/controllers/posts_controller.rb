dc7ea614f35c50589de327fba0cc5a16a1ad66b4
EC2 Default User <ec2-user@ip-172-31-2-46.ap-northeast-1.compute.internal>
2020-06-30 12:38:05 +0000

    ログイン機能実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,7 @@
 class PostsController < ApplicationController
+  
+  before_action :move_to_index, except: :index
+
   def index
     @posts = Post.all
   end
@@ -13,6 +16,10 @@ class PostsController < ApplicationController
   private
   def post_params
     params.permit(:name, :image, :text)
+  end
+  
+  def move_to_index
+    redirect_to action: :index unless user_signed_in?
   end
 
 end
